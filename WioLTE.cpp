c73fe4c5ec4472a2f5bf66453fc639ae9a48587b
matsujirushi <matsujirushi@live.jp>
2017-07-28 12:21:59 +0900

    Fix indent.

--- WioLTE.cpp
+++ WioLTE.cpp
@@ -9,39 +9,39 @@
 
 static void PinModeAndDefault(int pin, WiringPinMode mode)
 {
-  pinMode(pin, mode);
+	pinMode(pin, mode);
 }
 
 static void PinModeAndDefault(int pin, WiringPinMode mode, int value)
 {
-  pinMode(pin, mode);
-  if (mode == OUTPUT) digitalWrite(pin, value);
+	pinMode(pin, mode);
+	if (mode == OUTPUT) digitalWrite(pin, value);
 }
 
 static void DebugPrint(const char* data)
 {
-  char message[10];
-  int length = strlen(data);
-  
-  SerialUSB.print(length);
-  
-  SerialUSB.print(":");
-  
-  for (int i = 0; i < length; i++) {
-    if (data[i] >= 0x20)
-      SerialUSB.print((char)data[i]);
-    else
-      SerialUSB.print('.');
-  }
-  
-  SerialUSB.print(":");
-  
-  for (int i = 0; i < length; i++) {
-    sprintf(message, "%02x ", data[i]);
-    SerialUSB.print(message);
-  }
-  
-  SerialUSB.println("");
+	char message[10];
+	int length = strlen(data);
+
+	SerialUSB.print(length);
+
+	SerialUSB.print(":");
+
+	for (int i = 0; i < length; i++) {
+		if (data[i] >= 0x20)
+			SerialUSB.print((char)data[i]);
+		else
+			SerialUSB.print('.');
+	}
+
+	SerialUSB.print(":");
+
+	for (int i = 0; i < length; i++) {
+		sprintf(message, "%02x ", data[i]);
+		SerialUSB.print(message);
+	}
+
+	SerialUSB.println("");
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////
@@ -49,47 +49,47 @@ static void DebugPrint(const char* data)
 
 void WioLTE::DiscardRead()
 {
-  while (_Serial->available()) _Serial->read();
+	while (_Serial->available()) _Serial->read();
 }
 
 void WioLTE::SetTimeout(long timeout)
 {
-  _Timeout = timeout;
-  _Serial->setTimeout(_Timeout);
+	_Timeout = timeout;
+	_Serial->setTimeout(_Timeout);
 }
 
 bool WioLTE::ReadLine(char* data, int dataSize)
 {
-  int dataIndex = 0;
+	int dataIndex = 0;
 
-  Stopwatch sw;
-  while (dataIndex < dataSize - 1) {
-    sw.Start();
-    while (!_Serial->available()) {
-      if (sw.ElapsedMilliseconds() > _Timeout) {
-        data[dataIndex] = '\0';
-        return false; // Timeout.
-      }
-    }
-    int c = _Serial->read();
-    if (c < 0) {
-      data[dataIndex] = '\0';
-      return false; // No data.
-    }
-    if (c == '\r') continue;
-    
-    if (c == '\n') {
-      data[dataIndex] = '\0';
-      return true;
-    }
+	Stopwatch sw;
+	while (dataIndex < dataSize - 1) {
+		sw.Start();
+		while (!_Serial->available()) {
+			if (sw.ElapsedMilliseconds() > _Timeout) {
+				data[dataIndex] = '\0';
+				return false; // Timeout.
+			}
+		}
+		int c = _Serial->read();
+		if (c < 0) {
+			data[dataIndex] = '\0';
+			return false; // No data.
+		}
+		if (c == '\r') continue;
 
-    data[dataIndex++] = c;
-  }
+		if (c == '\n') {
+			data[dataIndex] = '\0';
+			return true;
+		}
 
-  if (dataIndex < dataSize) {
-    data[dataIndex] = '\0';
-  }
-  return false; // Overflow.
+		data[dataIndex++] = c;
+	}
+
+	if (dataIndex < dataSize) {
+		data[dataIndex] = '\0';
+	}
+	return false; // Overflow.
 }
 
 void WioLTE::Write(const char* str)
@@ -102,24 +102,23 @@ void WioLTE::Write(const char* str)
 
 void WioLTE::WriteCommand(const char* command)
 {
-  SerialUSB.print("<- ");
-  DebugPrint(command);
-  
-  _Serial->write(command);
-  _Serial->write('\r');
+	SerialUSB.print("<- ");
+	DebugPrint(command);
+
+	_Serial->write(command);
+	_Serial->write('\r');
 }
 
 bool WioLTE::WaitForResponse(const char* response)
 {
-  char data[MODULE_RESPONSE_MAX_SIZE];
-  do {
-    if (!ReadLine(data, sizeof (data))) return false;
-    SerialUSB.print("-> ");
-    DebugPrint(data);
-  }
-  while (strcmp(data, response) != 0);
+	char data[MODULE_RESPONSE_MAX_SIZE];
+	do {
+		if (!ReadLine(data, sizeof(data))) return false;
+		SerialUSB.print("-> ");
+		DebugPrint(data);
+	} while (strcmp(data, response) != 0);
 
-  return true;
+	return true;
 }
 
 bool WioLTE::WaitForResponse(const char* response, long timeout)
@@ -136,8 +135,8 @@ bool WioLTE::WaitForResponse(const char* response, long timeout)
 
 bool WioLTE::WriteCommandAndWaitForResponse(const char* command, const char* response)
 {
-  WriteCommand(command);
-  return WaitForResponse(response);
+	WriteCommand(command);
+	return WaitForResponse(response);
 }
 
 bool WioLTE::WriteCommandAndWaitForResponse(const char* command, const char* response, long timeout)
@@ -233,7 +232,7 @@ void WioLTE::PowerSupplyGrove(bool on)
 
 bool WioLTE::IsBusy() const
 {
-  return digitalRead(STATUS_PIN) ? true : false;
+	return digitalRead(STATUS_PIN) ? true : false;
 }
 
 bool WioLTE::TurnOnOrReset()
