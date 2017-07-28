3abeb31ba3e2de0ece0305bc51b657f73173e63f
matsujirushi <matsujirushi@live.jp>
2017-07-28 13:00:21 +0900

    Fix soracom-connect.ino.

--- examples/soracom-connect/soracom-connect.ino
+++ examples/soracom-connect/soracom-connect.ino
@@ -1,22 +1,5 @@
 #include <wiolte-driver.h>
 
-////////////////////////////////////////////////////////////////////////////////////////
-// Helper functions
-
-void PinModeAndDefault(int pin, WiringPinMode mode)
-{
-  pinMode(pin, mode);
-}
-
-void PinModeAndDefault(int pin, WiringPinMode mode, int value)
-{
-  pinMode(pin, mode);
-  if (mode == OUTPUT) digitalWrite(pin, value);
-}
-
-////////////////////////////////////////////////////////////////////////////////////////
-// setup and loop
-
 void setup() {
   delay(200);
 
@@ -24,77 +7,29 @@ void setup() {
   SerialUSB.println("--- START ---------------------------------------------------");
   
   WioLTE wio;
-  WioLTE::Stopwatch sw;
   
   SerialUSB.println("### I/O Initialize.");
-  PinModeAndDefault(WioLTE::MODULE_PWR_PIN , OUTPUT, LOW);
-  PinModeAndDefault(WioLTE::ANT_PWR_PIN    , OUTPUT, LOW);
-  PinModeAndDefault(WioLTE::ENABLE_VCCB_PIN, OUTPUT, LOW);
   wio.Init();
   
   SerialUSB.println("### Power supply ON.");
-  digitalWrite(WioLTE::MODULE_PWR_PIN , HIGH);    
-  digitalWrite(WioLTE::ANT_PWR_PIN    , LOW );  // Supply for GNSS antenna
-  digitalWrite(WioLTE::ENABLE_VCCB_PIN, HIGH);  // Supply for Grove
+  wio.PowerSupplyLTE(true);
   delay(5000);
 
-  if (!wio.IsBusy()) {
-    SerialUSB.println("### Reset.");
-    wio.Reset();
+  SerialUSB.println("### Turn on or reset.");
+  if (!wio.TurnOnOrReset()) {
+    SerialUSB.println("### ERROR! ###");
+    return;
   }
-  else {
-    SerialUSB.println("### Turn on.");
-    if (!wio.TurnOn()) {
-      SerialUSB.println("### ERROR! ###");
-      return;
-    }
-  }
-
-  SerialUSB.println("### Wait for ready.");
-  sw.Start();
-  while (!wio.WaitForResponse("RDY")) {
-    if (sw.ElapsedMilliseconds() >= 10000) {
-      SerialUSB.println("### ERROR! ###");
-      return;
-    }
-    SerialUSB.print(".");
-  }
-  SerialUSB.println("");
-  
-  SerialUSB.println("### Check AT command communication.");
-  sw.Start();
-  while (!wio.WriteCommandAndWaitForResponse("AT", "OK")) {
-    if (sw.ElapsedMilliseconds() >= 10000) {
-      SerialUSB.println("### ERROR! ###");
-      return;
-    }
-    SerialUSB.print(".");
-  }
-  SerialUSB.println("");
-
-  SerialUSB.println("### Echo off.");
-  if (!wio.WriteCommandAndWaitForResponse("ATE0", "OK")) SerialUSB.println("### ERROR! ###");
-
-  SerialUSB.println("### Wait for PIN.");
-  sw.Start();
-  while (!wio.WriteCommandAndWaitForResponse("AT+CPIN?", "OK")) {
-    if (sw.ElapsedMilliseconds() >= 10000) {
-      SerialUSB.println("### ERROR! ###");
-      return;
-    }
-    SerialUSB.print(".");
-  }
-  SerialUSB.println("");
   
   SerialUSB.println("### Connect the \"soracom.io\".");
   delay(5000);
-  if (!wio.WriteCommandAndWaitForResponse("AT+CREG?", "OK")) SerialUSB.println("### ERROR! ###");
-  if (!wio.WriteCommandAndWaitForResponse("AT+CGREG?", "OK")) SerialUSB.println("### ERROR! ###");
-  if (!wio.WriteCommandAndWaitForResponse("AT+CEREG?", "OK")) SerialUSB.println("### ERROR! ###");
-  if (!wio.WriteCommandAndWaitForResponse("AT+QIACT?", "OK")) SerialUSB.println("### ERROR! ###");
-  if (!wio.WriteCommandAndWaitForResponse("AT+QICSGP=1,1,\"soracom.io\",\"sora\",\"sora\",1", "OK")) SerialUSB.println("### ERROR! ###");
-  if (!wio.WriteCommandAndWaitForResponse("AT+QIACT=1", "OK")) SerialUSB.println("### ERROR! ###");
-  if (!wio.WriteCommandAndWaitForResponse("AT+QIACT?", "OK")) SerialUSB.println("### ERROR! ###");
+  if (!wio.WriteCommandAndWaitForResponse("AT+CREG?", "OK", 2000)) SerialUSB.println("### ERROR! ###");
+  if (!wio.WriteCommandAndWaitForResponse("AT+CGREG?", "OK", 2000)) SerialUSB.println("### ERROR! ###");
+  if (!wio.WriteCommandAndWaitForResponse("AT+CEREG?", "OK", 2000)) SerialUSB.println("### ERROR! ###");
+  if (!wio.WriteCommandAndWaitForResponse("AT+QIACT?", "OK", 2000)) SerialUSB.println("### ERROR! ###");
+  if (!wio.WriteCommandAndWaitForResponse("AT+QICSGP=1,1,\"soracom.io\",\"sora\",\"sora\",1", "OK", 2000)) SerialUSB.println("### ERROR! ###");
+  if (!wio.WriteCommandAndWaitForResponse("AT+QIACT=1", "OK", 2000)) SerialUSB.println("### ERROR! ###");
+  if (!wio.WriteCommandAndWaitForResponse("AT+QIACT?", "OK", 2000)) SerialUSB.println("### ERROR! ###");
 
   SerialUSB.println("### Finish.");
 }
