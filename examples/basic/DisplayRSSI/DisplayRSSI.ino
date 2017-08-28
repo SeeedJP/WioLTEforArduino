f64b6dc756119219a34563314b02d8a52ea1bf3a
matsujirushi <matsujirushi@live.jp>
2017-08-28 17:37:12 +0900

    Add sample.

---
+++ examples/basic/DisplayRSSI/DisplayRSSI.ino
@@ -0,0 +1,44 @@
+#include <wiolte-driver.h>
+#include <limits.h>
+
+#define INTERVAL  (5000)
+
+WioLTE Wio;
+
+void setup() {
+  delay(200);
+
+  SerialUSB.println("");
+  SerialUSB.println("--- START ---------------------------------------------------");
+  
+  SerialUSB.println("### I/O Initialize.");
+  Wio.Init();
+  
+  SerialUSB.println("### Power supply ON.");
+  Wio.PowerSupplyLTE(true);
+  delay(5000);
+
+  SerialUSB.println("### Turn on or reset.");
+  if (!Wio.TurnOnOrReset()) {
+    SerialUSB.println("### ERROR! ###");
+    return;
+  }
+  delay(3000);
+  
+  SerialUSB.println("Finish.");
+}
+
+void loop() {
+  SerialUSB.println("### Display RSSI.");
+  int rssi = Wio.GetReceivedSignalStrength();
+  if (rssi == INT_MIN) {
+    SerialUSB.println("### ERROR! ###");
+    goto err;
+  }
+  SerialUSB.print("RSSI:");
+  SerialUSB.print(rssi);
+  SerialUSB.println("");
+  
+err:
+  delay(INTERVAL);
+}
