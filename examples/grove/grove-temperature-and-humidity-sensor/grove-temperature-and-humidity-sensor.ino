ace58a8261767745b7c3b36edf74f04cc82ed0a5
matsujirushi <matsujirushi@live.jp>
2017-09-06 18:07:54 +0900

    Add sample for grove.

---
+++ examples/grove/grove-temperature-and-humidity-sensor/grove-temperature-and-humidity-sensor.ino
@@ -0,0 +1,123 @@
+#include <WioLTEforArduino.h>
+
+#define SENSOR_PIN    (WIOLTE_D38)
+
+void setup()
+{
+  TemperatureAndHumidityBegin(SENSOR_PIN);
+}
+
+void loop()
+{
+  float temp;
+  float humi;
+
+  if (!TemperatureAndHumidityRead(SENSOR_PIN, &temp, &humi)) {
+    SerialUSB.println("ERROR!");
+    goto err;
+  }
+
+  SerialUSB.print("Current humdity = ");
+  SerialUSB.print(humi);
+  SerialUSB.print("%  ");
+  SerialUSB.print("temperature = ");
+  SerialUSB.print(temp);
+  SerialUSB.println("C");
+
+err:
+  delay(2000);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//
+
+void TemperatureAndHumidityBegin(int pin)
+{
+  DHT11Init(pin);
+}
+
+bool TemperatureAndHumidityRead(int pin, float* temperature, float* humidity)
+{
+  byte data[5];
+  
+  DHT11Start(pin);
+  for (int i = 0; i < 5; i++) data[i] = DHT11ReadByte(pin);
+  DHT11Finish(pin);
+  
+  if(!DHT11Check(data, sizeof (data))) return false;
+  if (data[1] >= 10) return false;
+  if (data[3] >= 10) return false;
+
+  *humidity = (float)data[0] + (float)data[1] / 10.0f;
+  *temperature = (float)data[2] + (float)data[3] / 10.0f;
+
+  return true;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//
+
+void DHT11Init(int pin)
+{
+  digitalWrite(pin, HIGH);
+  pinMode(pin, OUTPUT);
+}
+
+void DHT11Start(int pin)
+{
+  // Host the start of signal
+  digitalWrite(pin, LOW);
+  delay(18);
+  
+  // Pulled up to wait for
+  pinMode(pin, INPUT);
+  while (!digitalRead(pin)) ;
+  
+  // Response signal
+  while (digitalRead(pin)) ;
+  
+  // Pulled ready to output
+  while (!digitalRead(pin)) ;
+}
+
+byte DHT11ReadByte(int pin)
+{
+  byte data = 0;
+  
+  for (int i = 0; i < 8; i++) {
+    while (digitalRead(pin)) ;
+
+    while (!digitalRead(pin)) ;
+    unsigned long start = micros();
+
+    while (digitalRead(pin)) ;
+    unsigned long finish = micros();
+
+    if ((unsigned long)(finish - start) > 50) data |= 1 << (7 - i);
+  }
+  
+  return data;
+}
+
+void DHT11Finish(int pin)
+{
+  // Releases the bus
+  while (!digitalRead(pin)) ;
+  digitalWrite(pin, HIGH);
+  pinMode(pin, OUTPUT);
+}
+
+bool DHT11Check(const byte* data, int dataSize)
+{
+  if (dataSize != 5) return false;
+
+  byte sum = 0;
+  for (int i = 0; i < dataSize - 1; i++) {
+    sum += data[i];
+  }
+
+  return data[dataSize - 1] == sum;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+
