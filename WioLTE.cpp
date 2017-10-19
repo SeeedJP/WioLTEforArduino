014b032db03ee73ae8c36874e31370905624a6a5
matsujirushi <matsujirushi@live.jp>
2017-10-19 16:24:03 +0900

    Change check sequence for network registration.

--- WioLTE.cpp
+++ WioLTE.cpp
@@ -542,48 +542,36 @@ bool WioLTE::Activate(const char* accessPointName, const char* userName, const c
 
 	sw.Start();
 	while (true) {
-		_Module.WriteCommand("AT+CREG?");
-		if ((parameter = _Module.WaitForResponse(NULL, 500, "+CREG: ", (ModuleSerial::WaitForResponseFlag)(ModuleSerial::WFR_START_WITH | ModuleSerial::WFR_REMOVE_START_WITH))) == NULL) continue;
-		parser.Parse(parameter);
-		if (parser.Size() < 2) return RET_ERR(false);
-		int resultCode = atoi(parser[0]);
-		int status = atoi(parser[1]);
-		if (_Module.WaitForResponse("OK", 500) == NULL) return RET_ERR(false);
-		if (status == 0) return RET_ERR(false);
-		if (status == 1 || status == 5) break;
+		int resultCode;
+		int status;
 
-		if (sw.ElapsedMilliseconds() >= 60000) return RET_ERR(false);
-	}
-
-	sw.Start();
-	while (true) {
 		_Module.WriteCommand("AT+CGREG?");
 		if ((parameter = _Module.WaitForResponse(NULL, 500, "+CGREG: ", (ModuleSerial::WaitForResponseFlag)(ModuleSerial::WFR_START_WITH | ModuleSerial::WFR_REMOVE_START_WITH))) == NULL) return RET_ERR(false);
 		parser.Parse(parameter);
 		if (parser.Size() < 2) return RET_ERR(false);
-		int resultCode = atoi(parser[0]);
-		int status = atoi(parser[1]);
+		resultCode = atoi(parser[0]);
+		status = atoi(parser[1]);
 		if (_Module.WaitForResponse("OK", 500) == NULL) return RET_ERR(false);
 		if (status == 0) return RET_ERR(false);
 		if (status == 1 || status == 5) break;
 
-		if (sw.ElapsedMilliseconds() >= 60000) return RET_ERR(false);
-	}
-
-	sw.Start();
-	while (true) {
 		_Module.WriteCommand("AT+CEREG?");
 		if ((parameter = _Module.WaitForResponse(NULL, 500, "+CEREG: ", (ModuleSerial::WaitForResponseFlag)(ModuleSerial::WFR_START_WITH | ModuleSerial::WFR_REMOVE_START_WITH))) == NULL) return RET_ERR(false);
 		parser.Parse(parameter);
 		if (parser.Size() < 2) return RET_ERR(false);
-		int resultCode = atoi(parser[0]);
-		int status = atoi(parser[1]);
+		resultCode = atoi(parser[0]);
+		status = atoi(parser[1]);
 		if (_Module.WaitForResponse("OK", 500) == NULL) return RET_ERR(false);
 		if (status == 0) return RET_ERR(false);
 		if (status == 1 || status == 5) break;
 
 		if (sw.ElapsedMilliseconds() >= 60000) return RET_ERR(false);
 	}
+
+	// for debug.
+	_Module.WriteCommandAndWaitForResponse("AT+CREG?", "OK", 500);
+	_Module.WriteCommandAndWaitForResponse("AT+CGREG?", "OK", 500);
+	_Module.WriteCommandAndWaitForResponse("AT+CEREG?", "OK", 500);
 
 	StringBuilder str;
 	if (!str.WriteFormat("AT+QICSGP=1,1,\"%s\",\"%s\",\"%s\",1", accessPointName, userName, password)) return RET_ERR(false);
