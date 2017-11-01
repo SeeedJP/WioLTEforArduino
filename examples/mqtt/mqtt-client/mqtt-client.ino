eeb179bbcd81b1bd416e82eba9e1745497c16736
matsujirushi <matsujirushi@live.jp>
2017-11-01 15:10:27 +0900

    Add sample.

---
+++ examples/mqtt/mqtt-client/mqtt-client.ino
@@ -0,0 +1,81 @@
+#include <WioLTEforArduino.h>
+#include <WioLTEClient.h>
+#include <PubSubClient.h>
+#include <stdio.h>
+
+#define APN               "soracom.io"
+#define USERNAME          "sora"
+#define PASSWORD          "sora"
+
+#define MQTT_SERVER_HOST  "hostname"
+#define MQTT_SERVER_PORT  (1883)
+
+#define ID                "WioLTE"
+#define OUT_TOPIC         "outTopic"
+#define IN_TOPIC          "inTopic"
+
+#define INTERVAL          (60000)
+
+WioLTE Wio;
+WioLTEClient WioClient(&Wio);
+PubSubClient MqttClient;
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  SerialUSB.print("Subscribe:");
+  for (int i = 0; i < length; i++) SerialUSB.print((char)payload[i]);
+  SerialUSB.println("");
+}
+
+void setup() {
+  delay(200);
+
+  SerialUSB.println("");
+  SerialUSB.println("--- START ---------------------------------------------------");
+  
+  SerialUSB.println("### I/O Initialize.");
+  Wio.Init();
+  
+  SerialUSB.println("### Power supply ON.");
+  Wio.PowerSupplyLTE(true);
+  delay(5000);
+
+  SerialUSB.println("### Turn on or reset.");
+  if (!Wio.TurnOnOrReset()) {
+    SerialUSB.println("### ERROR! ###");
+    return;
+  }
+
+  SerialUSB.println("### Connecting to \""APN"\".");
+  delay(5000);
+  if (!Wio.Activate(APN, USERNAME, PASSWORD)) {
+    SerialUSB.println("### ERROR! ###");
+    return;
+  }
+
+  SerialUSB.println("### Connecting to MQTT server \""MQTT_SERVER_HOST"\"");
+  MqttClient.setServer(MQTT_SERVER_HOST, MQTT_SERVER_PORT);
+  MqttClient.setCallback(callback);
+  MqttClient.setClient(WioClient);
+  if (!MqttClient.connect(ID)) {
+    SerialUSB.println("### ERROR! ###");
+    return;
+  }
+  MqttClient.subscribe(IN_TOPIC);
+}
+
+void loop() {
+  char data[100];
+  sprintf(data, "{\"uptime\":%lu}", millis() / 1000);
+  SerialUSB.print("Publish:");
+  SerialUSB.print(data);
+  SerialUSB.println("");
+  MqttClient.publish(OUT_TOPIC, data);
+  
+err:
+  unsigned long next = millis();
+  while (millis() < next + INTERVAL)
+  {
+    MqttClient.loop();
+  }
+}
+
