87f674a6e7e14a86267bfa421a02dbafb739d4b7
matsujirushi <matsujirushi@live.jp>
2017-11-24 10:53:39 +0900

    Add sample for grove. (#8)

---
+++ examples/grove/grove-rotary-angle-sensor/grove-rotary-angle-sensor.ino
@@ -0,0 +1,37 @@
+#include <WioLTEforArduino.h>
+
+#define ROTARY_ANGLE_PIN  (WIOLTE_A4)
+#define INTERVAL          (500)
+#define BAR_LENGTH        (40)
+
+WioLTE Wio;
+
+void setup() {
+  delay(200);
+
+  SerialUSB.println("");
+  SerialUSB.println("--- START ---------------------------------------------------");
+
+  SerialUSB.println("### I/O Initialize.");
+  Wio.Init();
+
+  SerialUSB.println("### Power supply ON.");
+  Wio.PowerSupplyGrove(true);
+  delay(5000);
+
+  SerialUSB.println("### Setup pin mode.");
+  pinMode(ROTARY_ANGLE_PIN, INPUT_ANALOG);
+}
+
+void loop() {
+  int rotaryAngle = analogRead(ROTARY_ANGLE_PIN);
+
+  int i;
+  for (i = 0; i < BAR_LENGTH * rotaryAngle / 4095; i++) SerialUSB.print("*");
+  for (; i < BAR_LENGTH; i++) SerialUSB.print(".");
+  SerialUSB.print(" ");
+  SerialUSB.println(rotaryAngle);
+  
+  delay(INTERVAL);
+}
+
